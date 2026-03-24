1bd3803ce3c8bf43cf4b7027457dcd080c5c0d94
ngbedebarnabas <ngbedebarnabas@gmail.com>
2026-03-24 15:55:29 +0100

    Implement Layout and Navigation

--- app/page.js
+++ app/page.js
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>The Wild Oasis, welcome to paradise.</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis, welcome to paradise.</h1>;
+      <Link href="/cabins">Explore Luxury Cabins</Link>
+    </div>
+  );
 }
