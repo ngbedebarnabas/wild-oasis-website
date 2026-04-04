2454f277a47d4ad939d019232d8d6c92635660bc
ngbedebarnabas <ngbedebarnabas@gmail.com>
2026-04-04 15:41:14 +0100

    Setup Google Sign In

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,1 +1,18 @@
 import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+const authConfig = {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+};
+
+export const {
+  auth,
+  handlers: { GET, POST },
+} = NextAuth(authConfig);
+
+// do auth config and api folder with the auth/route.js file in it.
