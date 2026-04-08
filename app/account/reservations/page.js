303fd7df59a3d9697dd8e624bcf1827c43e281ae
ngbedebarnabas <ngbedebarnabas@gmail.com>
2026-04-08 10:22:38 +0100

    Implement Optimistic delete with the useOptimistic hook

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,7 +1,6 @@
-import ReservationCard from "@/app/_components/ReservationCard";
+import ReservationList from "@/app/_components/ReservationList";
 import { auth } from "@/app/_lib/auth";
 import { getBookings } from "@/app/_lib/data-service";
-import { revalidatePath } from "next/cache";
 import Link from "next/link";
 
 export const metadata = {
@@ -26,11 +25,7 @@ export default async function Page() {
           </Link>
         </p>
       ) : (
-        <ul className="space-y-6">
-          {bookings.map((booking) => (
-            <ReservationCard booking={booking} key={booking.id} />
-          ))}
-        </ul>
+        <ReservationList bookings={bookings} />
       )}
     </div>
   );
